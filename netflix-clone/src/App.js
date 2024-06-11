3296c08b137cff59131f9e5663be2afe0f5d68b9
hhyeona <hhyeona0104@gmail.com>
2024-06-11 17:26:38 +0900

    feat: Movie Low 카테고리 생성하기

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,12 +1,39 @@
 import './App.css';
 import Banner from './components/Banner';
 import Nav from './components/Nav';
+import Row from './components/Row';
+import requests from './api/requests';
 
 function App() {
   return (
     <div className="App">
       <Nav></Nav>
       <Banner></Banner>
+      <Row title="NETFLIX ORIGINALS"
+        id="NO"
+        fetchUrl={requests.fectchNetflixOriginals}
+        isLarghRow>
+      </Row>
+      <Row title="Trending Now"
+        id="TN"
+        fetchUrl={requests.fetchTrending}
+        >
+      </Row>
+      <Row title="Top Rated"
+        id="TR"
+        fetchUrl={requests.fetchTopRated}
+        >
+      </Row>
+      <Row title="Action Movies"
+        id="AM"
+        fetchUrl={requests.fetchActionMovies}
+        >
+      </Row>
+      <Row title="Comedy Movies"
+        id="CM"
+        fetchUrl={requests.fetchComedyMovies}
+        >
+      </Row>
     </div>
   );
 }
---
+++ netflix-clone/src/components/Row.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+import axios from "../api/axios"
+import "./Row.css";
+
+export default function Row( {isLarghRow, title, id, fetchUrl}) {
+    const [movies, setMovies] = useState([]);
+    useEffect(() => {
+        fetchMovieData();
+    },[fetchUrl]);
+
+    const fetchMovieData = async () => {
+        const request = await axios.get(fetchUrl);
+        setMovies(request.data.results);
+        return request;
+    }
+
+  return (
+    // <div>why?</div>
+    <section className='row'>
+        <h2>{title}</h2>
+        <div className='slider'>
+            <div className='slider__arrow-left'>
+                <span className='arrow'>{"<"}</span>
+            </div>
+     
+        <div id={id} className='row__posters'>
+            {movies.map((movie) => (
+                <img 
+                key={movie.id} 
+                className={`row__poster ${isLarghRow && "row__posterLarge"}`} 
+                src={`https://image.tmdb.org/t/p/original/${isLarghRow ? movie.poster_path : movie.backdrop_path}`} 
+                alt={movie.name} />
+            ))}
+        </div>
+        <div className='slider__arrow-right'>
+            <span className='arrow'>
+                {">"}
+            </span>
+            </div>
+    </div>
+    </section>
+  )
+}
